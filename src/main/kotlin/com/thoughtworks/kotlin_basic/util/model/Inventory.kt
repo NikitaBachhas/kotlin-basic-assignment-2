444a606d9e7db4490f2b7dcbaa4a20b6f9e90341
NikitaBachhas <nikita.bachhas@thoughtworks.com>
2024-08-01 14:54:31 +0800

    [[Kotlin Assignment 2 - Request and Process Data] Added SerializedName to data models

--- src/main/kotlin/com/thoughtworks/kotlin_basic/util/model/Inventory.kt
+++ src/main/kotlin/com/thoughtworks/kotlin_basic/util/model/Inventory.kt
@@ -1,7 +1,14 @@
 package com.thoughtworks.kotlin_basic.util.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Inventory(
+    @SerializedName("SKU")
     val sku: String,
+
+    @SerializedName("stock")
     val stock: Int,
+
+    @SerializedName("region")
     val region: String
 )
--- src/main/kotlin/com/thoughtworks/kotlin_basic/util/model/Product.kt
+++ src/main/kotlin/com/thoughtworks/kotlin_basic/util/model/Product.kt
@@ -1,9 +1,20 @@
 package com.thoughtworks.kotlin_basic.util.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Product(
+    @SerializedName("SKU")
     val sku: String,
+
+    @SerializedName("name")
     val name: String,
+
+    @SerializedName("price")
     val price: Double,
+
+    @SerializedName("type")
     val type: String,
+
+    @SerializedName("imageUrl")
     val imageUrl: String
 )
