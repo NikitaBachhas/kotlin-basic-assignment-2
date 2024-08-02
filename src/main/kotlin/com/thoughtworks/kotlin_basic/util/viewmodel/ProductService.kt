68dc4d176e907d813d274045333782d780381c3f
NikitaBachhas <nikita.bachhas@thoughtworks.com>
2024-08-02 15:24:16 +0800

    [[Kotlin Assignment 2 - Request and Process Data] Removed suspend from code

--- src/main/kotlin/com/thoughtworks/kotlin_basic/util/viewmodel/ProductService.kt
+++ src/main/kotlin/com/thoughtworks/kotlin_basic/util/viewmodel/ProductService.kt
@@ -3,73 +3,60 @@ package com.thoughtworks.kotlin_basic.util.viewmodel
 import com.thoughtworks.kotlin_basic.util.model.Inventory
 import com.thoughtworks.kotlin_basic.util.model.Product
 import com.thoughtworks.kotlin_basic.util.model.ProductsAndInventoriesService
-import kotlinx.coroutines.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class ProductService {
 
-    suspend fun fetchAndDisplayProducts() {
+    fun fetchAndDisplayProducts() {
         val apiService = ProductsAndInventoriesService.getProductsAndInventoriesService()
 
-        val exceptionHandler = CoroutineExceptionHandler { CoroutineContext, throwable -> "Exception: ${throwable.localizedMessage}"}
+        val productsCall = apiService.getProducts()
+        val inventoriesCall = apiService.getInventories()
 
-         CoroutineScope(Dispatchers.IO + exceptionHandler).launch {
+        productsCall.enqueue(object : Callback<List<Product>> {
 
-            val productsCall = apiService.getProducts()
-            val inventoriesCall = apiService.getInventories()
+            override fun onResponse(call: Call<List<Product>>, response: Response<List<Product>>) {
 
-            withContext(Dispatchers.Main){
+                if (response.isSuccessful) {
+                    val products = response.body() ?: return
+                    inventoriesCall.enqueue(object : Callback<List<Inventory>> {
 
-                productsCall.enqueue(object : Callback<List<Product>> {
+                        override fun onResponse(call: Call<List<Inventory>>, response: Response<List<Inventory>>) {
 
-                    override fun onResponse(call: Call<List<Product>>, response: Response<List<Product>>) {
+                            if (response.isSuccessful) {
+                                val inventories = response.body() ?: return
+                                val productMap = products.associateBy { it.sku }
+                                val stockMap = inventories.groupBy { it.sku }
+                                    .mapValues { (_, inventories) -> inventories.sumOf { it.stock } }
 
-                        if (response.isSuccessful) {
-                            val products = response.body() ?: return
-                            inventoriesCall.enqueue(object : Callback<List<Inventory>> {
-
-                                override fun onResponse(call: Call<List<Inventory>>, response: Response<List<Inventory>>) {
-
-                                    if (response.isSuccessful) {
-                                        val inventories = response.body() ?: return
-                                        val productMap = products.associateBy { it.sku }
-                                        val stockMap = inventories.groupBy { it.sku }
-                                            .mapValues { (_, inventories) -> inventories.sumOf { it.stock } }
-
-                                        productMap.forEach { (sku, product) ->
-                                            val totalStock = stockMap[sku] ?: 0
-                                            val finalPrice = when {
-                                                totalStock > 100 -> product.price
-                                                totalStock in 31..100 -> product.price * 1.2
-                                                else -> product.price * 1.5
-                                            }
-                                            println("SKU: $sku, Name: ${product.name}, Price: $finalPrice, Stock: $totalStock, Image URL: ${product.imageUrl}")
-                                        }
+                                productMap.forEach { (sku, product) ->
+                                    val totalStock = stockMap[sku] ?: 0
+                                    val finalPrice = when {
+                                        totalStock > 100 -> product.price
+                                        totalStock in 31..100 -> product.price * 1.2
+                                        else -> product.price * 1.5
                                     }
-
-                                    else {
-                                        throw Exception("Error: ${response.message()}")
-                                    }
+                                    println("SKU: $sku, Name: ${product.name}, Price: $finalPrice, Stock: $totalStock, Image URL: ${product.imageUrl}")
                                 }
-
-                                override fun onFailure(call: Call<List<Inventory>>, t: Throwable) {
-                                    t.printStackTrace()
-                                }
-                            })
+                            } else {
+                                throw Exception("Error: ${response.message()}")
+                            }
                         }
 
-                        else {
-                            throw Exception("Error: ${response.message()}")
+                        override fun onFailure(call: Call<List<Inventory>>, t: Throwable) {
+                            t.printStackTrace()
                         }
-                    }
-
-                    override fun onFailure(call: Call<List<Product>>, t: Throwable) {
-                        t.printStackTrace()
-                    }
-                })
+                    })
+                } else {
+                    throw Exception("Error: ${response.message()}")
+                }
             }
-        }
+
+            override fun onFailure(call: Call<List<Product>>, t: Throwable) {
+                t.printStackTrace()
+            }
+        })
     }
 }
