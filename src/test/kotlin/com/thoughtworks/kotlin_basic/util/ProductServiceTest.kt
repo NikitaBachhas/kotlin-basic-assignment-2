191d0dfab01522cb6fd910a8369aadd0f81ecadc
NikitaBachhas <nikita.bachhas@thoughtworks.com>
2024-08-02 17:29:35 +0800

    [[Kotlin Assignment 2 - Request and Process Data] Added new test cases in ProductServiceTest.kt

--- src/test/kotlin/com/thoughtworks/kotlin_basic/util/ProductServiceTest.kt
+++ src/test/kotlin/com/thoughtworks/kotlin_basic/util/ProductServiceTest.kt
@@ -29,26 +29,92 @@ class ProductServiceTest {
     }
 
     @Test
-    fun testFetchAndDisplayProducts() {
-        @Test
-        fun `should return normal price if demand of product is normal` () {
-            val mockProductsFetched = listOf(
-                Product("ABC123", "Electronic Watch", 100.0, "NORMAL", "image1.jpg")
-            )
+    fun `fetchAndDisplayProducts - should return normal price if demand of product is normal`() {
+        val mockProductsFetched = listOf(
+            Product("ABC123", "Electronic Watch", 100.0, "NORMAL", "image1.jpg")
+        )
 
-            val mockInventoriesFetched = listOf(
-                Inventory("ABC123", 50, "Region1"),
-                Inventory("ABC123", 60, "Region2"),
-            )
+        val mockInventoriesFetched = listOf(
+            Inventory("ABC123", 50, "Region1"),
+            Inventory("ABC123", 60, "Region2"),
+        )
 
-            every { pISvc.getProducts().execute() } returns Response.success(mockProductsFetched)
-            every { pISvc.getInventories().execute() } returns Response.success(mockInventoriesFetched)
+        every { pISvc.getProducts().execute() } returns Response.success(mockProductsFetched)
+        every { pISvc.getInventories().execute() } returns Response.success(mockInventoriesFetched)
 
-            val output = captureConsoleOutput { productService.fetchAndDisplayProducts() }
+        val output = captureConsoleOutput { productService.fetchAndDisplayProducts() }
 
-            assertEquals(
+        println(output)
+
+        assertEquals(
             "SKU: ABC123, Name: Electronic Watch, Price: 100.0, Stock: 110, Image URL: image1.jpg",
-                output )
-        }
+            output
+        )
+    }
+
+    @Test
+    fun `fetchAndDisplayProducts - should return normal price if demand of product is high demand and stock is more than 100`() {
+        val mockProductsFetched = listOf(
+            Product("DEF456", "Sports Shoes", 120.0, "HIGH_DEMAND", "image2.jpg")
+        )
+
+        val mockInventoriesFetched = listOf(
+            Inventory("DEF456", 60, "Region1"),
+            Inventory("DEF456", 60, "Region2"),
+        )
+
+        every { pISvc.getProducts().execute() } returns Response.success(mockProductsFetched)
+        every { pISvc.getInventories().execute() } returns Response.success(mockInventoriesFetched)
+
+        val output = captureConsoleOutput { productService.fetchAndDisplayProducts() }
+
+        assertEquals(
+            "SKU: DEF456, Name: Sports Shoes, Price: 120.0, Stock: 120, Image URL: image2.jpg",
+            output
+        )
+    }
+
+    @Test
+    fun `fetchAndDisplayProducts - should return 120 percent price if demand of product is high demand and stock is between 30 and 100`() {
+        val mockProductsFetched = listOf(
+            Product("DEF456", "Sports Shoes", 120.0, "HIGH_DEMAND", "image2.jpg")
+        )
+
+        val mockInventoriesFetched = listOf(
+            Inventory("DEF456", 30, "Region1"),
+            Inventory("DEF456", 30, "Region2"),
+        )
+
+        every { pISvc.getProducts().execute() } returns Response.success(mockProductsFetched)
+        every { pISvc.getInventories().execute() } returns Response.success(mockInventoriesFetched)
+
+        val output = captureConsoleOutput { productService.fetchAndDisplayProducts() }
+
+        assertEquals(
+            "SKU: DEF456, Name: Sports Shoes, Price: 144.0, Stock: 60, Image URL: image2.jpg",
+            output
+        )
+    }
+
+    @Test
+    fun `fetchAndDisplayProducts - should return 150 percent price if demand of product is high demand and stock is less than 30`() {
+        val mockProductsFetched = listOf(
+            Product("DEF456", "Sports Shoes", 120.0, "HIGH_DEMAND", "image2.jpg")
+        )
+
+        val mockInventoriesFetched = listOf(
+            Inventory("DEF456", 10, "Region1"),
+            Inventory("DEF456", 10, "Region2"),
+        )
+
+        every { pISvc.getProducts().execute() } returns Response.success(mockProductsFetched)
+        every { pISvc.getInventories().execute() } returns Response.success(mockInventoriesFetched)
+
+        val output = captureConsoleOutput { productService.fetchAndDisplayProducts() }
+
+        assertEquals(
+            "SKU: DEF456, Name: Sports Shoes, Price: 180.0, Stock: 20, Image URL: image2.jpg",
+            output
+        )
     }
 }
